accc93f6bf6b668b3177f5f210223304152fe594
Diana <diana.brodovaya@gmail.com>
2020-07-09 22:47:53 +0700

    добавляет блок "Абонементы"

--- source/js/app.js
+++ source/js/app.js
@@ -19,15 +19,33 @@ for (let anchor of anchors) {
 /*Прячем элемент на маленьких экранах*/
 let hiddenElement = document.querySelector('.js-mobile-hidden');
 
-function windowResize () {
+function windowResize() {
   if (document.body.clientWidth < 1200) {
-    hiddenElement.style.display="none";
+    hiddenElement.style.display = "none";
   } else if (document.body.clientWidth >= 1200) {
-    hiddenElement.style.display="block";
+    hiddenElement.style.display = "block";
   }
 }
 
 window.addEventListener('resize', windowResize);
 
 windowResize();
+
+/*Табы*/
+let jsTriggers = document.querySelectorAll('.js-tab-trigger');
+
+jsTriggers.forEach(function(trigger) {
+  trigger.addEventListener('click', function() {
+    let id = this.getAttribute('data-tab'),
+      content = document.querySelector('.js-tab-content[data-tab="' + id + '"]'),
+      activeTrigger = document.querySelector('.js-tab-trigger.active'),
+      activeContent = document.querySelector('.js-tab-content.active');
+
+    activeTrigger.classList.remove('active');
+    trigger.classList.add('active');
+
+    activeContent.classList.remove('active');
+    content.classList.add('active');
+  });
+});
 
