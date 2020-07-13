62543fbe7edc81b4d93030766485432486e61ab1
Diana <diana.brodovaya@gmail.com>
2020-07-13 21:02:39 +0700

    добавляет правки по код-ревью

--- build/js/app.js
+++ build/js/app.js
@@ -1,28 +1,28 @@
 'use strict';
 
-/*Плавная прокрутка до якоря*/
-$(document).ready(function() {
-  $(".anchor").on("click", function(e) {
+/* Плавная прокрутка до якоря */
+$(document).ready(function () {
+  $('.anchor').on('click', function (e) {
     e.preventDefault();
-    let id = $(this).attr('href'),
-      top = $(id).offset().top;
+    var id = $(e.currentTarget).attr('href');
+    var top = $(id).offset().top;
     $('body,html').animate({scrollTop: top}, 800);
   });
 });
 
-/*Табы*/
-let jsTriggers = document.querySelectorAll('.js-tab-trigger');
+/* Табы */
+var jsTriggers = document.querySelectorAll('.js-tab-trigger');
 
-for (let i = 0; i < jsTriggers.length; i++) {
-  jsTriggers[i].addEventListener('click', function(e) {
+for (var i = 0; i < jsTriggers.length; i++) {
+  jsTriggers[i].addEventListener('click', function (e) {
     e.preventDefault();
-    let id = this.getAttribute('data-tab'),
-      content = document.querySelector('.js-tab-content[data-tab="' + id + '"]'),
-      activeTrigger = document.querySelector('.js-tab-trigger.active'),
-      activeContent = document.querySelector('.js-tab-content.active');
+    var id = e.currentTarget.getAttribute('data-tab');
+    var content = document.querySelector('.js-tab-content[data-tab="' + id + '"]');
+    var activeTrigger = document.querySelector('.js-tab-trigger.active');
+    var activeContent = document.querySelector('.js-tab-content.active');
 
     activeTrigger.classList.remove('active');
-    this.classList.add('active');
+    e.currentTarget.classList.add('active');
 
     activeContent.classList.remove('active');
     content.classList.add('active');
--- source/js/input-mask.js
+++ source/js/input-mask.js
@@ -1,32 +1,53 @@
-window.addEventListener('DOMContentLoaded', function() {
+'use strict';
+
+window.addEventListener('DOMContentLoaded', function () {
   function setCursorPosition(pos, elem) {
     elem.focus();
-    if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
-    else if (elem.createTextRange) {
-      let range = elem.createTextRange();
+    if (elem.setSelectionRange) {
+      elem.setSelectionRange(pos, pos);
+    } else if (elem.createTextRange) {
+      var range = elem.createTextRange();
       range.collapse(true);
       range.moveEnd('character', pos);
       range.moveStart('character', pos);
-      range.select()
+      range.select();
     }
   }
 
-  function mask(event) {
-    let matrix = '+7 (___) ___ ____',
-      i = 0,
-      def = matrix.replace(/\D/g, ''),
-      val = this.value.replace(/\D/g, '');
-    if (def.length >= val.length) val = def;
-    this.value = matrix.replace(/./g, function(a) {
-      return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a
+  function mask(e) {
+    var matrix = '+7 (___) ___ ____';
+    var i = 0;
+    var def = matrix.replace(/\D/g, '');
+    var val = e.currentTarget.value.replace(/\D/g, '');
+    if (def.length >= val.length) {
+      val = def;
+    }
+    e.currentTarget.value = matrix.replace(/./g, function (a) {
+      if (i >= val.length) {
+        if (/[_\d]/.test(a) && i < val.length) {
+          return val.charAt(i++);
+        } else {
+          return '';
+        }
+      } else {
+        if (/[_\d]/.test(a) && i < val.length) {
+          return val.charAt(i++);
+        } else {
+          return a;
+        }
+      }
     });
     if (event.type === 'blur') {
-      if (this.value.length === 2) this.value = ''
-    } else setCursorPosition(this.value.length, this)
+      if (e.currentTarget.value.length === 2) {
+        e.currentTarget.value = '';
+      } else {
+        setCursorPosition(e.currentTarget.value.length, e.currentTarget);
+      }
+    }
   }
 
-  let input = document.querySelectorAll('input[type="tel"]');
-  for (let i = 0; i < input.length; i++) {
+  var input = document.querySelectorAll('input[type="tel"]');
+  for (var i = 0; i < input.length; i++) {
     input[i].addEventListener('input', mask, false);
     input[i].addEventListener('focus', mask, false);
     input[i].addEventListener('blur', mask, false);
